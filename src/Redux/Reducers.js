04c835bb18dcaa4f36e62000d62c60744b86e0ac
s-mdhaffar <s.mdhaffar@gmail.com>
2021-12-19 21:32:37 +0100

    bootstrap commit

--- src/Redux/Reducers.js
+++ src/Redux/Reducers.js
@@ -1,18 +1,23 @@
-import { ADDTODO } from './ActionsTypes';
+import { ADDTODO, COMPLETE, DELETE, EDITTODO } from './ActionsTypes';
 
-const init = {
-	id     : '',
-	action : '',
-	isDone : ''
-};
+const init = [
+	{
+		id     : Math.random(),
+		action : 'Test',
+		isDone : false
+	}
+];
 
 const reducer = (state = init, { type, payload }) => {
 	switch (type) {
 		case ADDTODO:
-			return {
-				...state
-			};
-
+			return [ ...state, payload ];
+		case DELETE:
+			return state.filter((el) => el.id !== payload);
+		case COMPLETE:
+			return state.map((el) => (el.id === payload ? { ...el, isDone: !el.isDone } : el));
+		case EDITTODO:
+			return state.map((el) => (el.id === payload.id ? payload : el));
 		default:
 			return state;
 	}
